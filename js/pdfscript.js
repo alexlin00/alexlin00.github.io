5fdd2eee7a7556fc0fa85d066fb6dff9bd151836
alexlin00 <7alexlin@gmail.com>
2022-08-02 14:40:52 -0400

    added webpage files

---
+++ js/pdfscript.js
@@ -0,0 +1,12 @@
+//https://www.sanwebcorner.com/2016/07/load-pdfs-in-iframe-onclick-button.html
+//https://stackoverflow.com/questions/20235772/is-it-possible-to-open-a-new-window-and-embed-iframe-in-to-this
+
+
+function openPdf()
+{
+// var omyFrame = document.getElementById("myFrame");
+// omyFrame.style.display="block";
+// omyFrame.src = "docs/AL_Resume21.pdf";
+var win = window.open();
+win.document.write('<iframe width="100%" height="100%" src="docs/AL_Resume2022_642022.pdf" frameborder="0" allowfullscreen></iframe>')
+}
