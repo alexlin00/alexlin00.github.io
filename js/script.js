8d4a9a3ccb44d6af15813d40148f5990fd76a165
alexlin00 <105371496+alexlin00@users.noreply.github.com>
2023-08-10 17:30:48 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,5 +6,5 @@ function openPdf() {
 // omyFrame.style.display="block";
 // omyFrame.src = "docs/AL_Resume21.pdf";
 var win = window.open();
-win.document.write('<iframe width="100%" height="100%" src="https://www.dropbox.com/scl/fi/8lhghi7reic56wu1uopb3/AL_Resume6242022.pdf?raw=1" frameborder="0" allowfullscreen></iframe>')
+win.document.write('<iframe width="100%" height="100%" src="https://www.dropbox.com/scl/fi/8lhghi7reic56wu1uopb3/AL_Resume6242022.pdf?rlkey=2jc98npm8jf6rq070fsbq4vt5&raw=1" frameborder="0" allowfullscreen></iframe>')
 }
