bbf4b10e632ba8f07303b38e7107f2ae9471f8ce
alexlin00 <105371496+alexlin00@users.noreply.github.com>
2026-02-07 09:59:11 -0500

    Update dropbox resume link

--- js/script.js
+++ js/script.js
@@ -16,8 +16,7 @@ var win = window.open();
 //rlkey=56dp9d5wgwwht3hje73fzjr04&dl=0
 //}
 
-https://www.dropbox.com/scl/fi/s85405d2s0p1w9cxnsntd/Resume_Alex-2_6_2026-_b.pdf?rlkey=srqvfq5h9dtq58o1ouakk0dno&st=6n656wxr&dl=0
-win.document.write('<iframe width="100%" height="100%" src="https://dl.dropbox.com/scl/fi/s85405d2s0p1w9cxnsntd/Resume_Alex-2_6_2026-_b.pdf?rlkey=srqvfq5h9dtq58o1ouakk0dno&st=6n656wxr&dl=0" frameborder="0" allowfullscreen></iframe>')
+win.document.write('<iframe width="100%" height="100%" src="https://dl.dropbox.com/scl/fi/s85405d2s0p1w9cxnsntd/Resume_Alex-2_6_2026-_b.pdf?rlkey=srqvfq5h9dtq58o1ouakk0dno&st=mh9tgvcs&dl=0" frameborder="0" allowfullscreen></iframe>')
 //https://www.dropbox.com/scl/fi/sz1yxsdwr0150k2ti86vp/Resume_AL.pdf?
 //https://www.dropbox.com/scl/fi/ca3kbg4bj1hcybrcbynwb/Resume_AL.pdf?rlkey=fq0kg32p2v1blus160gdy0chh&dl=0
 //rlkey=56dp9d5wgwwht3hje73fzjr04&dl=0
